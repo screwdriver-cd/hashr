0144e354e112508199e5889548c37ba19af3fe68
Noah Katzman <nkatzman@yahoo-inc.com>
2016-06-22 14:31:19 -0700

    simple wrapper around the hasha module for standardizing around sha1 function

---
+++ index.js
@@ -0,0 +1,8 @@
+'use strict';
+const hasha = require('hasha');
+
+module.exports = {
+    sha1(dataToHash) {
+        return hasha(JSON.stringify(dataToHash), { algorithm: 'sha1', encoding: 'hex' });
+    }
+};
--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,59 @@
 'use strict';
 const assert = require('chai').assert;
+const sinon = require('sinon');
+const mockery = require('mockery');
+
+sinon.assert.expose(assert, { prefix: '' });
 
 describe('index test', () => {
-    it('fails', () => {
-        assert.isTrue(false);
+    let hashaMock;
+    let index;
+    let sha1Options;
+
+    before(() => {
+        mockery.enable({
+            useCleanCache: true,
+            warnOnUnregistered: false
+        });
+    });
+
+    beforeEach(() => {
+        hashaMock = sinon.stub();
+        mockery.registerMock('hasha', hashaMock);
+
+        /* eslint-disable global-require */
+        index = require('../');
+        /* eslint-enable global-require */
+
+        sha1Options = {
+            algorithm: 'sha1',
+            encoding: 'hex'
+        };
+    });
+
+    afterEach(() => {
+        mockery.deregisterAll();
+        mockery.resetCache();
+    });
+
+    after(() => {
+        mockery.disable();
+    });
+
+    it('handles objects', () => {
+        const object = {
+            value1: '1',
+            value2: '2'
+        };
+
+        index.sha1(object);
+        assert.calledWith(hashaMock, JSON.stringify(object), sha1Options);
+    });
+
+    it('handles strings', () => {
+        const str = 'foo';
+
+        index.sha1(str);
+        assert.calledWith(hashaMock, JSON.stringify(str), sha1Options);
     });
 });
