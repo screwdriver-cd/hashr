f3fd8da4a084593cf44be18b2989d2ddbff6c0fb
Dao Lam <daolam112@gmail.com>
2016-08-16 10:41:20 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 const hasha = require('hasha');
 
+// test
 module.exports = {
     sha1(dataToHash) {
         return hasha(JSON.stringify(dataToHash), { algorithm: 'sha1', encoding: 'hex' });
